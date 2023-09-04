4500df6dcbe03467f91485087bfa9672e275ae0a
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-09-04 20:22:23 -0300

    app - updates

--- app/database/factories/UserFactory.php
+++ app/database/factories/UserFactory.php
@@ -19,9 +19,25 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'birthday' => fake()->date(),
+            'city_birth' => fake()->city(),
+            'state_birth' => fake()->stateAbbr(),
+            'nationality' => fake()->countryCode(),
+            'cellphone' => fake()->cellphoneNumber(),
+            'landline' => fake()->landlineNumber(),
+            'street_address' => fake()->streetAddress(),
+            'building_number' => fake()->buildingNumber(),
+            'postcode' => fake()->postcode(),
+            'gerenal_record' => fake()->bothify('#.###.###-#'),
+            'individual_registration' => fake()->bothify('###.###.###-##'),
+            'driver_licence' => fake()->regexify('[0-9]{16}'),
+            'voter_registration' => fake()->regexify('[0-9]{16}'),
+            'social_security_card' => fake()->regexify('[0-9]{16}'),
+            'passaport_number' => Str::upper(Str::random(16)),
             'remember_token' => Str::random(10),
         ];
     }
--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,32 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name');
+            $table->string('username')->unique()->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('active')->default(true);
+            $table->boolean('confirmed')->default(false);
+            $table->binary('photo')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('city_birth')->nullable();
+            $table->string('state_birth')->nullable();
+            $table->string('nationality')->nullable();
+            $table->string('cellphone')->unique()->nullable();
+            $table->string('landline')->nullable();
+            $table->string('street_address')->nullable();
+            $table->string('building_number')->nullable();
+            $table->string('postcode')->nullable();
+            $table->string('gerenal_record')->unique()->nullable();
+            $table->string('individual_registration')->unique()->nullable();
+            $table->string('driver_licence')->unique()->nullable();
+            $table->string('voter_registration')->unique()->nullable();
+            $table->string('social_security_card')->unique()->nullable();
+            $table->string('passaport_number')->unique()->nullable();
+            $table->json('measurements')->nullable();
+            $table->json('dependents')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
