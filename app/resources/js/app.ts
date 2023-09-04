90058ea1be0d940c5629e73691827b1a03a13ba2
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-09-04 02:21:12 -0300

    app - updates

--- app/resources/js/app.ts
+++ app/resources/js/app.ts
@@ -5,8 +5,13 @@ import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createI18n } from 'vue-i18n'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const i18n = createI18n({
+    // something vue-i18n options here ...
+})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +20,8 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(i18n)
+
             .mount(el);
     },
     progress: {
