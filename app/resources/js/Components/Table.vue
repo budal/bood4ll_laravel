21400904c988402f6c276135fb2d0224be50f604
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-09-06 19:27:10 -0300

    app - updates

--- app/resources/js/Components/Table.vue
+++ app/resources/js/Components/Table.vue
@@ -5,14 +5,12 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
 import debounce from "lodash.debounce";
 import SearchInput from '@/Components/SearchInput.vue';
 
-defineProps<{
+const props = defineProps<{
     filters: any;
     items: any;
 }>();
 
 const value = ref("");
-
-// console.log(filters)
 
 const debouncedWatch = debounce(() => {
     router.visit('http://localhost/apps/users?search='+value.value, {
@@ -26,33 +24,41 @@ watch(value, debouncedWatch);
 onBeforeUnmount(() => {
   debouncedWatch.cancel();
 })
+
 </script>
 
 <template>
   <SearchInput placeholder="Search..." class="mt-3 w-96" :value="filters.search" v-model="value" />
   <div>
     <ul role="list" class="mt-2 divide-y divide-gray-100 dark:divide-gray-600">
-      <li v-for="item in items.data" :key="item.id" class="flex justify-between gap-x-6 px-3 py-3 hover:bg-gray-50 dark:hover:bg-gray-700">
-        <div class="flex min-w-0 gap-x-4">
-          <img class="h-12 w-12 flex-none rounded-full bg-gray-50 dark:bg-gray-600" :src="item.imageUrl" alt="" />
-          <div class="min-w-0 flex-auto">
-            <p class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100">{{ item.name }}</p>
-            <p class="mt-1 truncate text-xs leading-5 text-gray-600 dark:text-gray-400">{{ item.username }} / {{ item.email }}</p>
-          </div>
-        </div>
-        <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-          <p class="text-sm leading-6 text-gray-900 dark:text-gray-100">{{ item.role }}</p>
-          <p v-if="item.lastSeen" class="mt-1 text-xs leading-5 text-gray-600 dark:text-gray-400">
-            Last seen <time :datetime="item.lastSeenDateTime">{{ item.lastSeen }}</time>
-          </p>
-          <div v-else class="mt-1 flex items-center gap-x-1.5">
-            <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-              <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+      <template v-for="(item, key) in items.data">
+        <li
+          v-if="items.data[key].name[0] != items.data[key-1]?.name[0]"
+          class="sticky top-[60px] px-4 py-3 flex items-center font-semibold text-sm text-slate-900 dark:text-slate-200 bg-slate-50/90 dark:bg-slate-700/90 backdrop-blur-sm ring-1 ring-slate-900/10 dark:ring-black/10">
+          {{ items.data[key].name[0] }}
+        </li>
+        <li class="flex justify-between gap-x-6 px-3 py-3 hover:bg-gray-50 dark:hover:bg-gray-700">
+          <div class="flex min-w-0 gap-x-4">
+            <img class="h-12 w-12 flex-none rounded-full bg-gray-50 dark:bg-gray-600" :src="item.imageUrl" alt="" />
+            <div class="min-w-0 flex-auto">
+              <p class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100">{{ item.name }}</p>
+              <p class="mt-1 truncate text-xs leading-5 text-gray-600 dark:text-gray-400">{{ item.username }} / {{ item.email }}</p>
             </div>
-            <p class="text-xs leading-5 text-gray-600 dark:text-gray-400">Online</p>
           </div>
-        </div>
-      </li>
+          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+            <p class="text-sm leading-6 text-gray-900 dark:text-gray-100">{{ item.role }}</p>
+            <p v-if="item.lastSeen" class="mt-1 text-xs leading-5 text-gray-600 dark:text-gray-400">
+              Last seen <time :datetime="item.lastSeenDateTime">{{ item.lastSeen }}</time>
+            </p>
+            <div v-else class="mt-1 flex items-center gap-x-1.5">
+              <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+              </div>
+              <p class="text-xs leading-5 text-gray-600 dark:text-gray-400">Online</p>
+            </div>
+          </div>
+        </li>
+      </template>
       <li v-if="items.data.length === 0" class="flex justify-between gap-x-6 px-3 py-3">
         <p class="text-sm leading-6 text-gray-400 dark:text-gray-600">No items to show.</p>
       </li>
@@ -82,7 +88,7 @@ onBeforeUnmount(() => {
               <span class="sr-only">Previous</span>
               <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
             </Link>
-            <span v-for="(item, key) in items.links">
+            <span v-for="item in items.links">
               <Link
                 v-if="item.label > 0 && item.label != items.current_page"
                 :key="item.key" :href="item.url" :active="item.active" class="relative inline-flex items-center px-4 py-2 text-sm font-semibold bg-gray-800 dark:bg-gray-200 text-white dark:text-gray-800 ring-1 ring-inset ring-gray-600 dark:ring-gray-300 hover:bg-gray-700 dark:hover:bg-white focus:z-20 focus:outline-offset-0">
