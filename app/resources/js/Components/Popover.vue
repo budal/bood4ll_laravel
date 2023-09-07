f1fae8f6c24f2b414f6e3d081d1345d7755ff3ca
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-09-06 22:31:49 -0300

    app - updates

--- app/resources/js/Components/Popover.vue
+++ app/resources/js/Components/Popover.vue
@@ -39,23 +39,17 @@ const classes = computed(() =>
         leave-from-class="translate-y-0 opacity-100"
         leave-to-class="translate-y-1 opacity-0"
       >
-        <PopoverPanel
-          class="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl"
-        >
-          <div
-            class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5"
-          >
-            <div class="grid gap-8 bg-white dark:bg-gray-800 p-7 lg:grid-cols-2">
+        <PopoverPanel class="absolute left-1/2 z-10 mt-3 -translate-x-1/2 transform px-4 sm:px-0 w-screen max-w-xl md:max-w-3xl lg:max-w-5xl" >
+          <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5" >
+            <div class="grid gap-8 bg-white dark:bg-gray-800 p-7 md:grid-cols-2 lg:grid-cols-3">
               <Link
                 v-for="item in items"
                 :key="item.name"
                 :href="route(item.route)"
                 class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
               >
-                <div
-                  class="flex h-20 w-20 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12"
-                >
-                <div v-html="item.icon" class="h-8 w-8 flex-none text-gray-400" aria-hidden="true"></div>
+                <div class="flex h-20 w-20 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12" >
+                  <div v-html="item.icon" class="h-8 w-8 flex-none text-gray-400" aria-hidden="true"></div>
                 </div>
                 <div class="ml-4">
                   <p class="text-sm font-medium text-gray-900 dark:text-gray-100">
