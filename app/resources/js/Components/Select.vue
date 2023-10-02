084ce053178e16cd0af04f93aa8bff746ddd0a5e
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-10-01 22:08:14 -0300

    app - updates

--- app/resources/js/Components/Select.vue
+++ app/resources/js/Components/Select.vue
@@ -6,38 +6,47 @@
     ListboxOptions,
     ListboxOption,
   } from '@headlessui/vue'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid';
-
-const props = defineProps<{
-    modelValue: Object | string | number;
-    content: any;
-}>(); 
-
-const emit = defineEmits(['update:modelValue']);
-
-const selectedPerson = ref(props.content[0])
+  import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid';
 
 
-console.log(props.modelValue)
+  const props = withDefaults(
+      defineProps<{
+        modelValue: Object | string | number;
+        content: any;
+        multiple?: boolean;
+      }>(),
+      {
+        multiple: false,
+      }
+  );
+
+  const emit = defineEmits(['update:modelValue']);
+
+  const selectedOptions = props.content[props.content.map(function(e: any) { return e.id; }).indexOf(props.modelValue)];
+  const selectedOptionsRef = props.multiple == true ? ref([selectedOptions]) : ref(selectedOptions);
 </script>
 
 <template>
   <Listbox 
-    v-model="selectedPerson"
+    v-model="selectedOptionsRef"
+    :multiple="multiple"
   >
     <div class="relative mt-1">
       <ListboxButton
         class="w-full block cursor-default rounded-md bg-white py-2.5 pl-3 pr-10 text-left sm:text-sm shadow-sm focus:outline-none dark:bg-gray-900 dark:text-gray-300 border border-gray-300 dark:border-gray-500 focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800 focus:ring-gray-300 dark:focus:ring-gray-500 disabled:opacity-25 transition ease-in-out duration-500"
         v-slot="{ value }"
       >
-        <span class="block truncate">{{ $t(value.title) }}</span>
+        <span class="block truncate">
+          {{  
+            multiple == true ? (
+              value.map((item: any) => item.title).join(', ')
+            ) : $t(value.title)
+          }}
+        </span>
         <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2" >
           <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
         </span>
       </ListboxButton>
-
-
-
       <transition
         enter-active-class="transition duration-100 ease-out"
         enter-from-class="transform scale-95 opacity-0"
@@ -46,50 +55,41 @@ console.log(props.modelValue)
         leave-from-class="transform scale-100 opacity-100"
         leave-to-class="transform scale-95 opacity-0"
       >
-
-
-      <ListboxOptions
-        class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-lg sm:text-sm bg-white dark:bg-gray-900 dark:text-gray-300 border border-gray-300 dark:border-gray-500 focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800 focus:ring-gray-300 dark:focus:ring-gray-500 disabled:opacity-25 transition ease-in-out duration-500"
-      >
-        <ListboxOption
-          v-for="person in props.content"
-          as="template"
-          :key="person.id"
-          :value="person"
-          :disabled="person.disabled"
-          v-slot="{ active, selected }"
+        <ListboxOptions
+          class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-lg sm:text-sm bg-white dark:bg-gray-900 dark:text-gray-300 border border-gray-300 dark:border-gray-500 focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800 focus:ring-gray-300 dark:focus:ring-gray-500 disabled:opacity-25 transition ease-in-out duration-500"
         >
-          <li
-            :class="[
-                active ? 'bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-300' : 'text-gray-700 dark:text-gray-300',
-                'relative cursor-default select-none py-2 pl-10 pr-4',
-              ]"
+          <ListboxOption
+            v-for="item in content"
+            as="template"
+            :key="item.id"
+            :value="item"
+            :disabled="item.disabled"
+            v-slot="{ active, selected }"
           >
-            <span
+            <li
               :class="[
-                selected ? 'font-medium' : 'font-normal',
-                person.disabled ? 'opacity-50 dark:opacity-25 italic line-through' : '',
-                'block truncate',
-              ]"
-              >{{ $t(person.title) }}
-            </span>
-            <span
-              v-if="selected"
-              class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600 dark:text-gray-300"
+                  active ? 'bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-300' : 'text-gray-700 dark:text-gray-300',
+                  'relative cursor-default select-none py-2 pl-10 pr-4',
+                ]"
             >
-              <CheckIcon class="h-5 w-5" aria-hidden="true" />
-            </span>
-          </li>
-        </ListboxOption>
-      </ListboxOptions>
-
-
-    </transition>
-
-
-
-
-
+              <span
+                :class="[
+                  selected ? 'font-medium' : 'font-normal',
+                  item.disabled ? 'opacity-50 dark:opacity-25 italic line-through' : '',
+                  'block truncate',
+                ]"
+                >{{ $t(item.title) }}
+              </span>
+              <span
+                v-if="selected"
+                class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600 dark:text-gray-300"
+              >
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
     </div>
   </Listbox>
 </template>
