1ab826d6921fe36efe4c5f291083e7b277327db5
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-10-27 23:44:19 -0300

    app - updates

--- app/resources/js/Components/NavPopover.vue
+++ app/resources/js/Components/NavPopover.vue
@@ -9,23 +9,25 @@ const props = defineProps<{
     active?: boolean;
 }>();
 
+const routeCurrent = window.location.href;
+
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'
+        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-info-light dark:border-info-dark text-sm font-medium leading-5 text-primary-dark dark:text-primary-light focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-primary-dark dark:text-primary-light hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'
 );
 </script>
 
 <template>
   <div>
     <Popover v-slot="{ open }">
-      <PopoverButton class="ring-0">
+      <PopoverButton class="ring-0 ">
         <div :class=classes>
           <span class="ml-1 py-5">
             <slot />
           </span>
           <ChevronDownIcon
-            class="ml-2 h-5 w-5 text-grey-300 transition duration-150 ease-in-out group-hover:text-opacity-80"
+            class="ml-2 h-5 w-5 text-grey-300 transition duration-150 ease-in-out"
             aria-hidden="true"
           />
         </div>
@@ -46,8 +48,10 @@ const classes = computed(() =>
                 v-for="item in items"
                 :key="item.name"
                 :href="((route(item.route) as unknown) as string)"
-                class="flex items-center rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none ring-1 ring-gray-100 dark:ring-gray-700 focus-visible:ring focus-visible:ring-gray-100 focus-visible:ring-opacity-50 transition ease-in-out duration-500"
+                class="flex items-center rounded-lg p-2 bg-primary-dark-hover hover:bg-primary-dark dark:bg-primary-light dark:hover:bg-primary-light-hover focus:outline-none ring-1 ring-gray-100 dark:ring-gray-700 focus-visible:ring focus-visible:ring-gray-100 focus-visible:ring-opacity-50 transition ease-in-out duration-500"
+                :class="routeCurrent.includes((route(item.route) as unknown) as string) ? 'ring-info-light dark:ring-info-dark' : ''"
               >
+                {{  }}
                 <div class="flex h-20 w-20 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12" >
                   <div v-html="item.icon" class="h-8 w-8 flex-none text-gray-400" aria-hidden="true"></div>
                 </div>
