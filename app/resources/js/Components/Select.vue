b768d7a8649bdf465bedae17eebfaba406a3cd11
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-11-04 08:45:21 -0300

    app - updates

--- app/resources/js/Components/Select.vue
+++ app/resources/js/Components/Select.vue
@@ -2,6 +2,7 @@
   import { ref } from 'vue'
   import { ComboboxAnchor, ComboboxContent, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxItemIndicator, ComboboxLabel, ComboboxRoot, ComboboxSeparator, ComboboxTrigger, ComboboxViewport } from 'radix-vue'
   import { Icon } from '@iconify/vue'
+import { computed } from 'vue';
 
   const props = withDefaults(
     defineProps<{
@@ -19,18 +20,22 @@
 
   const selectedContent = ref(props.modelValue)
 
-  function filterFunction(list: typeof props.content[number], searchTerm: string) {
-    return list.filter((item: { title: string }) => {
-      return item.title.toLowerCase().includes(searchTerm.toLowerCase())
-    })
-  }
+  const searchTerm = ref('')
+
+  const filteredContent = computed(() =>
+    searchTerm.value === ''
+      ? props.content
+      : props.content.filter((items: {id: string, title: string}) => {
+        return items.title.toLowerCase().includes(searchTerm.value.toLowerCase())
+      })
+  )
 </script>
 
 <template>
   <ComboboxRoot 
     class="relative" 
     v-model="selectedContent" 
-    :filter-function="filterFunction"
+    v-model:searchTerm="searchTerm"
     :multiple="multiple"
   >
     <ComboboxAnchor class="w-full inline-flex">
@@ -41,6 +46,10 @@
           :placeholder="multiple ? $t('Select one or more options') : $t('Select an option')" 
           class="w-full block p-2 placeholder:text-sm placeholder-primary-dark/20 dark:placeholder-primary-dark/20 bg-zero-light dark:bg-zero-dark text-zero-light dark:text-zero-dark rounded-md border border-zero-light dark:border-zero-dark rounded-full focus:outline-none focus:ring-2 focus:ring-primary-light dark:focus:ring-primary-dark focus:ring-offset-1 focus:ring-offset-primary-light dark:focus:ring-offset-primary-dark cursor-pointer transition ease-in-out duration-500 disabled:opacity-25 " 
         />
+        <!-- <span class="flex items-center text-primary-light dark:text-primary-dark absolute inset-y-2 px-2 rounded-md text-sm p-1 bg-primary-light dark:bg-primary-dark ring-0">
+          <p class="">123</p>
+          <Icon icon="mdi:window-close" class="w-4 h-4 ml-1 text-primary-light dark:text-primary-dark" />
+        </span> -->
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
           <Icon icon="mdi:chevron-down" class="w-6 h-6 text-primary-light dark:text-primary-dark" />
         </div>
@@ -50,8 +59,11 @@
       <ComboboxViewport class="p-[5px] max-h-60">
         <ComboboxEmpty class="text-xs font-medium text-center py-2" />
         <ComboboxGroup>
+          <ComboboxLabel as="span" class="px-[25px] text-xs leading-[25px] text-mauve11">
+            Fruits
+          </ComboboxLabel>
           <ComboboxItem
-            v-for="item in props.content" 
+            v-for="item in filteredContent" 
             :key="item.id"
             :value="item.id"
             :disabled="item.disabled"
