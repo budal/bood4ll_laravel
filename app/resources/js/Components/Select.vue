6eac058a311a7e8ec314c70b6a36e121bb025cf8
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-11-07 19:58:06 -0300

    app - updates

--- app/resources/js/Components/Select.vue
+++ app/resources/js/Components/Select.vue
@@ -98,7 +98,7 @@ import { watch } from 'vue';
     </ComboboxAnchor>
     <ComboboxContent class="absolute w-full mt-1 min-w-[160px] bg-white overflow-hidden bg-zero-light dark:bg-zero-dark text-zero-light dark:text-zero-dark rounded-md border border-zero-light dark:border-zero-dark rounded-full focus:outline-none focus:ring-2 focus:ring-primary-light dark:focus:ring-primary-dark focus:ring-offset-1 focus:ring-offset-primary-light dark:focus:ring-offset-primary-dark">
       <ComboboxViewport class="p-[5px] max-h-60">
-        <ComboboxEmpty class="text-xs font-medium text-center" />
+        <ComboboxEmpty class="text-xs font-medium text-center">{{ $t('No items to show.') }}</ComboboxEmpty>
         <ComboboxGroup>
           <ComboboxItem
             v-for="item in filteredContent" 
@@ -114,6 +114,9 @@ import { watch } from 'vue';
               {{ $t(item.title) }}
             </span>
           </ComboboxItem>
+          <div class="text-xs w-full font-medium text-center">
+            <span v-if="filteredContent.length < 1 && searchTerm.length == 0">{{ $t('No items to show.') }}</span>
+          </div>
         </ComboboxGroup>
       </ComboboxViewport>
     </ComboboxContent>
