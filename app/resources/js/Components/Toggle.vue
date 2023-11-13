ce1505ad9b261459cc3a16457e4ced10105f46a1
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-11-13 19:12:19 -0300

    app - updates

--- app/resources/js/Components/Toggle.vue
+++ app/resources/js/Components/Toggle.vue
@@ -9,7 +9,7 @@ const toggleState = ref(false)
 <template>
   <Toggle
     v-model:pressed="toggleState" aria-label="Toggle italic"
-    class="min-h-[41px] w-full flex items-center justify-center rounded text-base leading-4 bg-zero-light dark:bg-zero-dark hover:bg-success-light-hover dark:hover:bg-success-dark-hover text-zero-light dark:text-zero-dark hover:text-success-light dark:hover:text-success-dark data-[state=on]:bg-success-light data-[state=on]:text-success-light             border border-zero-light dark:border-zero-dark         shadow-primary-light/20 dark:shadow-primary-dark/20 shadow-[0_2px_10px] focus-within:shadow-[0_0_0_2px] focus-within:shadow-primary-light dark:focus-within:shadow-primary-dark "
+    class="min-h-[41px] w-full flex items-center justify-center rounded text-base leading-4 bg-zero-light dark:bg-zero-dark hover:bg-success-light-hover dark:hover:bg-success-dark-hover text-zero-light dark:text-zero-dark hover:text-success-light dark:hover:text-success-dark data-[state=on]:bg-success-light data-[state=on]:text-success-light border border-zero-light dark:border-zero-dark focus:outline-none focus:ring-2 focus:ring-primary-light dark:focus:ring-primary-dark focus:ring-offset-1 focus:ring-offset-primary-light dark:focus:ring-offset-primary-dark shadow-primary-light/20 dark:shadow-primary-dark/20 shadow-[0_2px_10px] focus-within:shadow-[0_0_0_2px] focus-within:shadow-primary-light dark:focus-within:shadow-primary-dark transition ease-in-out duration-500 disabled:opacity-25"
   >
     <Icon icon="radix-icons:font-italic" class="w-[15px] h-[15px]" />
   </Toggle>
