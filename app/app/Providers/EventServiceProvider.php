75b6b85db6dda73a3af26f1b372c89c18dd73e36
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-11-13 23:16:44 -0300

    app - updates

--- app/app/Providers/EventServiceProvider.php
+++ app/app/Providers/EventServiceProvider.php
@@ -25,7 +25,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResponseFactory::macro('modal', function ($modal) { 
+            inertia()->share(['modal' => $modal]); 
+        });
     }
 
     /**
