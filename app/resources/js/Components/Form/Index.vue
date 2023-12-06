a39dc2878f3bb5e12a63bafd7b98312970a20d5b
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-12-06 02:05:54 -0300

    app - updates

--- app/resources/js/Components/Form/Index.vue
+++ app/resources/js/Components/Form/Index.vue
@@ -61,106 +61,98 @@
 <template>
     <Tabs :items="form" :tabs="true">
         <template v-for="mkForm in form" v-slot:[mkForm.id]>
-            <section 
-                v-if="mkForm.condition !== false" 
-            >
-                <form @submit.prevent="sendForm(mkForm.id)" class="space-y-6">
-                    <div class="flex flex-col">
-                        <template v-for="group in mkForm.fields">
-                            <div :class="`grid sm:grid-cols-${mkForm.cols} sm:gap-4`">
-                                <template v-for="field in group">
-                                    <div :class="`${field.span ? `sm:col-span-${field.span}` : ''}`">
-                                        <InputLabel 
-                                            v-if="field.title"
-                                            as="span" 
-                                            :for="field.name" 
-                                            :value="$t(field.title)" 
-                                            :required="field.required" 
-                                        />
+            <form v-if="mkForm.condition !== false" @submit.prevent="sendForm(mkForm.id)" class="space-y-6">
+                <div v-for="group in mkForm.fields" :class="`grid sm:grid-cols-${mkForm.cols} sm:gap-4`">
+                    <template v-for="field in group">
+                        <div :class="`${field.span ? `sm:col-span-${field.span}` : ''}`">
+                            <InputLabel 
+                                v-if="field.title"
+                                as="span" 
+                                :for="field.name" 
+                                :value="$t(field.title)" 
+                                :required="field.required" 
+                            />
 
-                                        <TextInput 
-                                            v-if="field.type == 'input' || field.type == 'date' || field.type == 'email'"
-                                            :id="field.name"
-                                            :name="field.name"
-                                            :type="field.type"
-                                            :mask="field.mask"
-                                            class="mt-1"
-                                            v-model="jsForm[field.name]"
-                                            :disabled="field.disabled ? field.disabled : data?.inalterable === true"
-                                            :required="field.required"
-                                            :autocomplete="field.autocomplete"
-                                            :autofocus="field.autofocus"
-                                        />
-                
-                                        <Toggle 
-                                            v-if="field.type == 'toggle'"
-                                            :id="field.name"
-                                            :name="field.name"
-                                            :type="field.type"
-                                            :colorOn="field.colorOn"
-                                            :colorOff="field.colorOff"
-                                            :rotate="field.rotate"
-                                            :disabled="field.disabled ? field.disabled : data?.inalterable === true"
-                                            class="mt-1"
-                                            v-model="jsForm[field.name]"
-                                            @keydown.enter.prevent
-                                            @click.prevent
-                                        />
-                
-                                        <Select 
-                                            v-if="field.type == 'select'" 
-                                            :id="field.name"
-                                            :name="field.name"
-                                            :content="field.content"
-                                            class="mt-1 block w-full"
-                                            :class="{ 'opacity-25': data?.inalterable === true }"
-                                            v-model="jsForm[field.name]"
-                                            :disabled="field.disabled ? field.disabled : data?.inalterable === true"
-                                            :required="field.required"
-                                            :autocomplete="field.autocomplete"
-                                            :multiple="field.multiple"
-                                            @keydown.enter.prevent
-                                        />
-                
-                                        <Table 
-                                            v-if="field.type == 'table'"
-                                            :prefix="mkForm.id"
-                                            :id="field.name"
-                                            :name="field.name"
-                                            :shortcutKey="field.shortcutKey"
-                                            :menu="field.content.menu" 
-                                            :routes="field.content.routes" 
-                                            :items="field.content.items" 
-                                            :titles="field.content.titles" 
-                                        />
-                        
-                                        <InputError class="mt-2" :message="jsForm.errors[field.name]" />
-                                    </div>
-                                </template>
-                            </div>
-                        </template>
-                    </div>
+                            <TextInput 
+                                v-if="field.type == 'input' || field.type == 'date' || field.type == 'email'"
+                                :id="field.name"
+                                :name="field.name"
+                                :type="field.type"
+                                :mask="field.mask"
+                                class="mt-1"
+                                v-model="jsForm[field.name]"
+                                :disabled="field.disabled ? field.disabled : data?.inalterable === true"
+                                :required="field.required"
+                                :autocomplete="field.autocomplete"
+                                :autofocus="field.autofocus"
+                            />
+    
+                            <Toggle 
+                                v-if="field.type == 'toggle'"
+                                :id="field.name"
+                                :name="field.name"
+                                :type="field.type"
+                                :colorOn="field.colorOn"
+                                :colorOff="field.colorOff"
+                                :rotate="field.rotate"
+                                :disabled="field.disabled ? field.disabled : data?.inalterable === true"
+                                class="mt-1"
+                                v-model="jsForm[field.name]"
+                                @keydown.enter.prevent
+                                @click.prevent
+                            />
+    
+                            <Select 
+                                v-if="field.type == 'select'" 
+                                :id="field.name"
+                                :name="field.name"
+                                :content="field.content"
+                                class="mt-1 block w-full"
+                                :class="{ 'opacity-25': data?.inalterable === true }"
+                                v-model="jsForm[field.name]"
+                                :disabled="field.disabled ? field.disabled : data?.inalterable === true"
+                                :required="field.required"
+                                :autocomplete="field.autocomplete"
+                                :multiple="field.multiple"
+                                @keydown.enter.prevent
+                            />
+    
+                            <Table 
+                                v-if="field.type == 'table'"
+                                :prefix="mkForm.id"
+                                :id="field.name"
+                                :name="field.name"
+                                :shortcutKey="field.shortcutKey"
+                                :menu="field.content.menu" 
+                                :routes="field.content.routes" 
+                                :items="field.content.items" 
+                                :titles="field.content.titles" 
+                            />
             
-                    <div v-if="props.routes[mkForm.id]" class="flex items-center gap-4">
-                        <Button 
-                            color="primary" 
-                            :class="{ 'opacity-25': jsForm.processing }"
-                            :disabled="data?.inalterable === true || jsForm.processing"
-                        >
-                            {{ $t('Save') }}
-                        </Button>
+                            <InputError class="mt-2" :message="jsForm.errors[field.name]" />
+                        </div>
+                    </template>
+                </div>
         
-                        <Transition
-                            enter-active-class="transition ease-in-out"
-                            enter-from-class="opacity-0"
-                            leave-active-class="transition ease-in-out"
-                            leave-to-class="opacity-0"
-                        >
-                            <p v-if="jsForm.recentlySuccessful" class="text-sm text-zero-light/50 dark:text-zero-dark/50">{{ $t('Saved.') }}</p>
-                        </Transition>
-                    </div>
-                </form>
-            </section>
+                <div v-if="props.routes[mkForm.id]" class="flex items-center gap-4">
+                    <Button 
+                        color="primary" 
+                        :class="{ 'opacity-25': jsForm.processing }"
+                        :disabled="data?.inalterable === true || jsForm.processing"
+                    >
+                        {{ $t('Save') }}
+                    </Button>
+    
+                    <Transition
+                        enter-active-class="transition ease-in-out"
+                        enter-from-class="opacity-0"
+                        leave-active-class="transition ease-in-out"
+                        leave-to-class="opacity-0"
+                    >
+                        <p v-if="jsForm.recentlySuccessful" class="text-sm text-zero-light/50 dark:text-zero-dark/50">{{ $t('Saved.') }}</p>
+                    </Transition>
+                </div>
+            </form>
         </template>
     </Tabs>
 </template>
--- app/resources/js/Components/Tabs.vue
+++ app/resources/js/Components/Tabs.vue
@@ -1,45 +1,63 @@
 <script setup lang="ts">
   import { TabsContent, TabsIndicator, TabsList, TabsRoot, TabsTrigger } from 'radix-vue'
+  import { computed } from 'vue';
+  import { ref } from 'vue';
 
-  defineProps<{
+  const props = defineProps<{
     items: any;
+    modelValue?: any;
     tabs?: boolean;
   }>();    
 
+  const emit = defineEmits(['update:modelValue']);
+
+  // emit('update:modelValue', props.items[0].id)
+
+  // const defaultTab = computed(() => props.modelValue)
+  
+  const onChangeTab = (tab: string) => {
+    // emit('update:modelValue', tab)
+    console.log(tab)
+  }
 </script>
 
 <template>
-  <TabsRoot v-if="tabs === true" class="overflow-hidden rounded-xl p-2 sm:p-8 bg-zero-light dark:bg-zero-dark sm:rounded-lg shadow-primary-light/20 dark:shadow-primary-dark/20 shadow-[0_2px_10px]" :default-value="`${items[0].id}`">
+  <TabsRoot 
+    v-if="tabs === true" 
+    :default-value="props.items[0].id"
+    @update:modelValue="onChangeTab"
+    class="overflow-hidden rounded-xl p-2 sm:p-8 bg-zero-light dark:bg-zero-dark sm:rounded-lg shadow-primary-light/20 dark:shadow-primary-dark/20 shadow-[0_2px_10px]" 
+  >
     <TabsList class="relative shrink-0 flex border-b border-zero-light dark:border-zero-dark">
       <TabsIndicator class="absolute px-8 left-0 h-[3px] bottom-0 w-[--radix-tabs-indicator-size] translate-x-[--radix-tabs-indicator-position] rounded-full transition-[width,transform] duration-300">
         <div class="bg-primary-light dark:bg-primary-dark w-full h-full" />
       </TabsIndicator>
-      <TabsTrigger v-for="item in items"
-        class="bg-zero-light dark:bg-zero-dark text-zero-light/50 dark:text-zero-dark/50 px-5 h-[45px] flex-1 flex items-center justify-center leading-none select-none hover:text-secondary-light hover:dark:text-secondary-dark data-[state=active]:bg-zero-light-hover data-[state=active]:dark:bg-zero-dark-hover data-[state=active]:text-zero-light data-[state=active]:dark:text-zero-dark outline-none cursor-pointer transition ease-in-out duration-500"
-        :value="`${item.id}`"
-      >
-        {{ $t(item.title) }}
-      </TabsTrigger>
+      <template v-for="item in items">
+        <TabsTrigger
+          v-if="item.condition !== false"
+          class="bg-zero-light dark:bg-zero-dark text-zero-light/50 dark:text-zero-dark/50 px-5 h-[45px] flex-1 flex items-center justify-center leading-none select-none hover:text-secondary-light hover:dark:text-secondary-dark data-[state=active]:bg-zero-light-hover data-[state=active]:dark:bg-zero-dark-hover data-[state=active]:text-zero-light data-[state=active]:dark:text-zero-dark outline-none cursor-pointer transition ease-in-out duration-500"
+          :value="`${item.id}`"
+        >
+          {{ $t(item.title) }}
+        </TabsTrigger>
+      </template>
     </TabsList>
     <TabsContent
       v-for="item in items"
       class="grow bg-zero-light dark:bg-zero-dark rounded-b-md outline-none"
       :value="`${item.id}`"
     >
-      <div>
-        <header v-if="item.title || item.subtitle" class="mb-2">
-            <h2 v-if="item.title" class="text-lg font-medium text-zero-light dark:text-zero-dark">{{ $t(item.title) }}</h2>
-            <p v-if="item.subtitle" class="mt-1 text-sm text-zero-light/50 dark:text-zero-dark/50">{{ $t(item.subtitle) }}</p>
-        </header>
-        <p class="text-zero-light dark:text-zero-dark !leading-normal">
+      <header v-if="item.title || item.subtitle" class="mb-2">
+        <h2 v-if="item.title" class="text-lg font-medium text-zero-light dark:text-zero-dark">{{ $t(item.title) }}</h2>
+        <p v-if="item.subtitle" class="mt-1 text-sm text-zero-light/50 dark:text-zero-dark/50">{{ $t(item.subtitle) }}</p>
+      </header>
+      <section>
           <slot :name="`${item.id}`" />
-        </p>
-      </div>
-
+      </section>
     </TabsContent>
   </TabsRoot>
 
-  <div class="grid grid-cols-1 gap-10">
+  <div v-else class="grid grid-cols-1 gap-10">
     <template v-for="item in items">
       <section 
         v-if="item.condition !== false" 
