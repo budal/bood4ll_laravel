f525bea23e07652fdb5dfeffbc34d00a2693f402
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-12-08 11:00:25 -0300

    app - updates

--- app/resources/js/Components/Toast.vue
+++ app/resources/js/Components/Toast.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { ToastAction, ToastDescription, ToastProvider, ToastRoot, ToastTitle, ToastViewport } from 'radix-vue'
+import { ToastAction, ToastClose, ToastDescription, ToastProvider, ToastRoot, ToastTitle, ToastViewport } from 'radix-vue'
+import { ProgressIndicator, ProgressRoot } from 'radix-vue'
+import { onMounted, ref } from 'vue'
 
 const open = ref(false)
 const eventDateRef = ref(new Date())
@@ -24,6 +25,13 @@ function handleClick() {
     open.value = true
   }, 100)
 }
+
+const progressValue = ref(10)
+
+onMounted(() => {
+  const timer = setTimeout(() => (progressValue.value = 66), 3000)
+  return () => clearTimeout(timer)
+})
 </script>
 
 <template>
@@ -40,8 +48,11 @@ function handleClick() {
   >
   <ToastRoot
       v-model:open="open"
-      class="bg-secondary-light dark:bg-secondary-dark text-secondary-light dark:text-secondary-dark rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+      class="relative bg-secondary-light dark:bg-secondary-dark text-secondary-light dark:text-secondary-dark rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
     >
+    <ToastClose aria-label="Close" class="absolute top-0 right-2">
+      <span aria-hidden>×</span>
+    </ToastClose>
       <ToastTitle class="[grid-area:_title] mb-[5px] font-medium text-slate12 text-[15px]">
         Scheduled: Catch up
       </ToastTitle>
@@ -52,6 +63,16 @@ function handleClick() {
         >
           {{ prettyDate(eventDateRef) }}
         </time>
+        <ProgressRoot
+          v-model="progressValue"
+          class="relative overflow-hidden bg-primary-light rounded-full w-full sm:w-[300px] h-1"
+          style="transform: translateZ(0)"
+        >
+          <ProgressIndicator
+            class="bg-info-light rounded-full w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+            :style="`transform: translateX(-${100 - progressValue}%)`"
+          />
+        </ProgressRoot>
       </ToastDescription>
       <ToastAction class="[grid-area:_action]" as-child alt-text="Goto schedule to undo">
         <button class="inline-flex items-center justify-center rounded font-medium text-xs px-[10px] leading-[25px] h-[25px] bg-green2 text-green11 shadow-[inset_0_0_0_1px] shadow-green7 hover:shadow-[inset_0_0_0_1px] hover:shadow-green8 focus:shadow-[0_0_0_2px] focus:shadow-green8">
