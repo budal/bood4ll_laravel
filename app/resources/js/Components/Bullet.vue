5c5519210dbacc9a58c11517c6b30fe4ee0ab03f
Thiago Philipe Budal <budal.thiago@gmail.com>
2023-12-08 18:53:56 -0300

    app - updates

--- app/resources/js/Components/Bullet.vue
+++ app/resources/js/Components/Bullet.vue
@@ -2,8 +2,8 @@
   <span class="relative inline-flex">
     <slot />
     <span class="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
-      <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-      <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+      <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-info-light bg-info-dark opacity-75"></span>
+      <span class="relative inline-flex rounded-full h-3 w-3 bg-info-light text-info-light text-xs"></span>
     </span>
   </span>
 </template>
