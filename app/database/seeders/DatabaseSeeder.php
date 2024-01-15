1ff6efbb93abd9ea6fbd8fb6c88b4594ca9957ba
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-01-15 12:23:13 -0300

    app - updates

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             SuperadminUserSeeder::class,
             CalendarSeeder::class,
+            AbsencesTypesSeeder::class
         ]);
     }
 }
