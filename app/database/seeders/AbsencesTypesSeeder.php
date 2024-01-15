c54ff5f62e6f55d494fe26b7998cc38217141401
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-01-15 11:52:40 -0300

    app - updates

--- app/database/seeders/AbsencesTypesSeeder.php
+++ app/database/seeders/AbsencesTypesSeeder.php
@@ -12,10 +12,17 @@ class AbsencesTypesSeeder extends Seeder
         $superAdmin = \App\Models\User::first();
 
         $absencesTypes = [
-            ['name' => 'Confraternização Universal', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 5, 'working_days' => true],
+            ['name' => 'Férias', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 30, 'working_days' => false],
+            ['name' => 'Férias (operador de raio-X)', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 20, 'working_days' => false],
+            ['name' => 'Dispensa por conta das férias', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 30, 'working_days' => false],
+            ['name' => 'Licença para tratamento da própria saúde', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 670, 'working_days' => true],
+            ['name' => 'Licença para tratamento da saúde de pessoa da família', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 670, 'working_days' => true],
+            ['name' => 'Licença para tratamento de interesses particulares', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 670, 'working_days' => true],
+            ['name' => 'Licença especial', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 0, 'working_days' => true],
+            ['name' => 'Licença capacitação', 'owner' => $superAdmin->id, 'active' => true, 'duration' => 0, 'working_days' => true],
         ];
 
-        \App\Models\Holiday::factory(count($absencesTypes))
+        \App\Models\AbsencesType::factory(count($absencesTypes))
             ->state(new Sequence(...$absencesTypes))
             ->create();
     }
