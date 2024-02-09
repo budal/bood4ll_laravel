512f62219689c80daa367c04bf61041b34fcc930
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-02-09 17:18:42 -0300

    app - updates

--- app/app/Models/Holiday.php
+++ app/app/Models/Holiday.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Holiday extends Base
@@ -18,11 +17,6 @@ class Holiday extends Base
         'starts_at',
         'ends_at',
     ];
-
-    public function calendars(): BelongsToMany
-    {
-        return $this->belongsToMany(Calendar::class);
-    }
 
     protected function startAt(): Attribute
     {
