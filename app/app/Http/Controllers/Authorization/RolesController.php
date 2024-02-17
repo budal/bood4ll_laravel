0b33217918ff0f393dd408b44a48ea59d6cf5542
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-02-17 03:19:48 -0300

    app - updates

--- app/app/Http/Controllers/Authorization/RolesController.php
+++ app/app/Http/Controllers/Authorization/RolesController.php
@@ -45,16 +45,16 @@ class RolesController extends Controller
                 'abilities',
                 'users' => function ($query) use ($request) {
                     $query->when($request->user()->cannot('isSuperAdmin', User::class), function ($query) use ($request) {
-                        // $query->join('unit_user', function (JoinClause $join) use ($request, $query) {
-                        //     $join->on('unit_user.user_id', '=', 'role_user.user_id')
-                        //         ->whereIn('unit_user.unit_id', $request->user()->unitsIds());
+                        $query->join('unit_user', function (JoinClause $join) use ($request, $query) {
+                            $join->on('unit_user.user_id', '=', 'role_user.user_id')
+                                ->where('unit_user.primary', true);
 
-                        //     //         if ($request->user()->cannot('hasFullAccess', User::class)) {
-                        //     //             $query->where('unit_user.user_id', $request->user()->id);
-                        //     //         }
+                            if ($request->user()->cannot('hasFullAccess', User::class)) {
+                                $query->where('unit_user.user_id', $request->user()->id);
+                            }
 
-                        //     //         $query->whereIn('unit_user.unit_id', $request->user()->unitsIds());
-                        // });
+                            $query->whereIn('unit_user.unit_id', $request->user()->unitsIds());
+                        });
                     });
                 }
             ])
