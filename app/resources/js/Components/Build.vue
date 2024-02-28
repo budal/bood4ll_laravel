b814811eadebbc235757fa840140436a74c5d3d7
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-02-28 13:58:05 -0300

    app - updates

--- app/resources/js/Components/Build.vue
+++ app/resources/js/Components/Build.vue
@@ -1,145 +1,19 @@
 <script setup lang="ts">
-import { isValidUrl, getData, toast } from "@/helpers";
-import { Ref, nextTick, ref } from "vue";
+import { isValidUrl, getData } from "@/helpers";
+import { nextTick, ref } from "vue";
 import { Link, useForm } from "@inertiajs/vue3";
 
-import { inject, onMounted } from "vue";
-
-import TabView from "primevue/tabview";
-import TabPanel from "primevue/tabpanel";
-import type { DynamicDialogInstance } from "primevue/dynamicdialogoptions";
-
 import Table from "@/Components/Table.vue";
-import { watch } from "vue";
 
 const props = withDefaults(
     defineProps<{
         component: any;
         buildRoute?: string;
-
-        ///////
-        data?: any;
-        formRoute?: any;
-        create?: any;
-        form?: any;
-        routes?: any;
-        shortcutKey?: string;
-        tabs?: boolean;
-        status?: string | null;
-        statusTheme?: string | null;
     }>(),
-    {
-        tabs: true,
-        statusTheme: "info",
-    },
+    {},
 );
 
-interface FormItems {
-    [key: string]: string;
-}
-
 const formValue = ref([]);
-
-let formItems: FormItems = {};
-
-// props.component.forEach((form: any) => {
-// form.fields.forEach((field: any) => {
-// console.log(field);
-// fields.forEach((field: any) => {
-//         formItems[field.name] = props.data
-//             ? props?.data[field.name] || ""
-//             : "";
-// });
-//     });
-// });
-
-const jsForm = useForm(formItems);
-
-const sendForm = (formId: string) => {
-    // console.log(jsForm.data());
-    // return false;
-
-    jsForm.submit(props.routes[formId].method, props.routes[formId].route, {
-        preserveScroll: true,
-        onSuccess: () => {
-            toast();
-        },
-        onError: () => {
-            toast();
-
-            console.log(jsForm.errors);
-
-            // if (jsForm.errors.password) {
-            //     jsForm.reset('password', 'password_confirmation');
-            //     passwordInput.value?.focus();
-            // }
-            // if (jsForm.errors.current_password) {
-            //     jsForm.reset('current_password');
-            //     currentPasswordInput.value?.focus();
-            // }
-        },
-        onFinish: () => {
-            props.routes[formId].reset === true
-                ? props.routes[formId].fieldsToReset
-                    ? jsForm.reset(...props.routes[formId].fieldsToReset)
-                    : jsForm.reset()
-                : false;
-        },
-    });
-};
-
-// let component = ref("");
-
-// const changeTab = (item: any) => {
-//     component.value = item || null;
-// };
-
-// modal
-let modalInfo = ref();
-
-const showModal = ref(false);
-
-const passwordInput = ref<HTMLInputElement | null>(null);
-
-const modalForm = useForm({
-    password: "",
-});
-
-const openModal = (field: any) => {
-    modalInfo.value = field;
-    showModal.value = true;
-
-    nextTick(() => passwordInput.value?.focus());
-};
-
-const closeModal = () => {
-    showModal.value = false;
-
-    modalForm.reset();
-};
-
-const submitModal = () => {
-    modalForm.submit(
-        modalInfo.value.method,
-        isValidUrl(modalInfo.value.route) as string,
-        {
-            preserveScroll: true,
-            preserveState: modalInfo.value.preserveState,
-
-            onSuccess: () => {
-                toast();
-                closeModal();
-            },
-            onError: () => {
-                toast();
-                () => passwordInput.value?.focus();
-            },
-            onFinish: () => {
-                modalForm.reset();
-            },
-        },
-    );
-};
 
 const onFormDataLoad = () => {
     if (props.buildRoute) {
@@ -161,11 +35,7 @@ const onFormDataLoad = () => {
         aria-live="polite"
         aria-label="Content loaded after page scrolled down"
     >
-        <form
-            v-if="component.showIf !== false"
-            @submit.prevent="sendForm(component.id)"
-            class="space-y-6"
-        >
+        <form v-if="component.showIf !== false" class="space-y-6">
             <div
                 class="grid sm:gap-2"
                 :class="{
