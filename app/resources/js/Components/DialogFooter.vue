ab68b0f4ff25fa889fbec0b22b293f539fbb96a0
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-03-07 00:31:41 -0300

    app - updates

--- app/resources/js/Components/DialogFooter.vue
+++ app/resources/js/Components/DialogFooter.vue
@@ -1,61 +1,89 @@
 <script setup lang="ts">
-import { Ref, inject } from "vue";
+import { Ref, inject, ref } from "vue";
 import { useConfirm } from "primevue/useconfirm";
 import { useToast } from "primevue/usetoast";
-
-interface DialogRef {
-    close: (event?: any) => void;
-}
+import { trans } from "laravel-vue-i18n";
 
 const confirm = useConfirm();
 const toast = useToast();
-const dialogRef = inject<Ref<DialogRef>>("dialogRef");
+const dialogRef: Ref<any> | undefined = inject("dialogRef");
 
-const close = (event: any) => {
-    if (dialogRef && dialogRef.value) {
+const data = ref(dialogRef?.value.data.action);
+
+const close = (event: Event) => {
+    if (dialogRef?.value) {
         dialogRef.value.close(event);
     }
 };
 
-const confirmDialog = (event: any) => {
-    confirm.require({
-        group: "popup",
-        target: event.currentTarget,
-        message: "Do you want to delete this record?",
-        icon: "pi pi-info-circle",
-        rejectClass: "p-button-secondary p-button-outlined p-button-sm",
-        acceptClass: "p-button-danger p-button-sm",
-        rejectLabel: "Cancel",
-        acceptLabel: "Delete",
-        accept: () => {
-            toast.add({
-                severity: "info",
-                summary: "Confirmed",
-                detail: "Record deleted",
-                life: 3000,
-            });
-
-            close(event);
-        },
-    });
+const send = (event: Event) => {
+    console.log(1);
+    if (data.value) {
+        toast.add({
+            severity: data.value.toastClass || "info",
+            summary: trans(data.value.toastTitle || "Confirmed"),
+            detail: trans(data.value.toast || null),
+            life: 3000,
+        });
+    }
+    close(event);
 };
+
+const handleConfirm = (event: Event) => {
+    if (data.value?.confirm === true) {
+        confirmDialog(event);
+    } else {
+        send(event);
+    }
+};
+
+const confirmDialog = (event: Event) => {
+    if (data.value) {
+        confirm.require({
+            group: "popup",
+            target:
+                event.currentTarget instanceof HTMLElement
+                    ? event.currentTarget
+                    : undefined,
+            message: trans(
+                data.value.popup || "Do you want confirm this action?",
+            ),
+            icon: data.value.popupIcon || "pi pi-info-circle",
+            rejectClass:
+                data.value.popupCancelClass ||
+                "p-button-secondary p-button-outlined p-button-sm",
+            acceptClass:
+                data.value.popupConfirmClass || "p-button-info p-button-sm",
+            rejectLabel: trans(data.value.popupCancel || "No"),
+            acceptLabel: trans(data.value.popupConfirm || "Yes"),
+            accept: () => send(event),
+        });
+    }
+};
+
+const dialogCancelLabel = trans(data.value?.dialogCancel || "Cancel");
+const dialogCancelIcon = data.value?.dialogCancelIcon || "pi pi-times";
+const dialogConfirmLabel = trans(data.value?.dialogConfirm || "Confirm");
+const dialogConfirmClass = data.value?.dialogConfirmClass || "info";
+const dialogConfirmIcon = data.value?.dialogConfirmIcon || "pi pi-check";
 </script>
 
 <template>
     <div class="flex justify-content-end gap-2">
         <Button
             type="button"
-            :label="$t('Cancel')"
+            :label="dialogCancelLabel"
             severity="secondary"
-            icon="pi pi-times"
-            @click="close($event)"
+            :icon="dialogCancelIcon"
+            @click="close"
             autofocus
         />
         <Button
             type="button"
-            :label="$t('Confirm')"
-            icon="pi pi-check"
-            @click="confirmDialog($event)"
+            :label="dialogConfirmLabel"
+            :severity="dialogConfirmClass"
+            :icon="dialogConfirmIcon"
+            @click="handleConfirm"
         />
     </div>
 </template>
