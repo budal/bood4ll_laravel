dff4062ed57f91a6467d1d220310fc9199cb15ae
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-03-21 11:26:00 -0300

    app - updates

--- app/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ app/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -10,7 +10,9 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->string('tokenable_type', 255);
+            $table->uuid('tokenable_id');
+            // $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
