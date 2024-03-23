94b31006b3bfc2209131cb22f129f5bbd01ed1a6
Thiago Philipe Budal <budal.thiago@gmail.com>
2024-03-22 22:56:42 -0300

    apps - updates

--- app/app/Http/Controllers/HolidaysController.php
+++ app/app/Http/Controllers/HolidaysController.php
@@ -7,6 +7,7 @@ use App\Models\Calendar;
 use App\Models\Holiday;
 use App\Models\User;
 use Emargareten\InertiaModal\Modal;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -380,5 +381,278 @@ class HolidaysController extends Controller
                 'toast_count' => count($request->list),
             ]);
         }
+    }
+
+
+
+
+
+
+
+
+    public function deleteUserDestroy(Request $request): JsonResponse
+    {
+        // $this->authorize('access', User::class);
+        // $this->authorize('isManager', User::class);
+        // $this->authorize('canDestroyOrRestore', [Role::class, $request]);
+
+        $list = collect($request->list)->pluck('id');
+
+        try {
+            $total = User::whereIn('id', $list)->delete();
+
+            return response()->json([
+                'type' => 'success',
+                'title' => 'Users',
+                'message' => '{0} Nothing to remove.|[1] Item removed successfully.|[2,*] :total items successfully removed.',
+                'length' => $total,
+                'replacements' => ['total' => $total],
+            ]);
+        } catch (\Throwable $e) {
+            report($e);
+
+            return response()->json([
+                'type' => 'error',
+                'message' => 'Error on remove selected item.|Error on remove selected items.',
+                'length' => count($list),
+            ]);
+        }
+    }
+
+    public function postUserRestore(Request $request): JsonResponse
+    {
+        // $this->authorize('access', User::class);
+        // $this->authorize('isManager', User::class);
+        // $this->authorize('canDestroyOrRestore', [Role::class, $request]);
+
+        $list = collect($request->list)->pluck('id');
+
+        try {
+            $total = User::whereIn('id', $list)->restore();
+
+            return response()->json([
+                'type' => 'success',
+                'title' => 'Users',
+                'message' => '{0} Nothing to restore.|[1] Item restored successfully.|[2,*] :total items successfully restored.',
+                'length' => $total,
+                'replacements' => ['total' => $total],
+            ]);
+        } catch (\Throwable $e) {
+            report($e);
+
+            return response()->json([
+                'type' => 'error',
+                'message' => 'Error on remove selected item.|Error on remove selected items.',
+                'length' => count($list),
+            ]);
+        }
+    }
+
+    public function deleteUserForceDestroy(Request $request): JsonResponse
+    {
+        $this->authorize('access', User::class);
+        $this->authorize('isSuperAdmin', User::class);
+
+        $list = collect($request->list)->pluck('id');
+
+        try {
+            $total = User::whereIn('id', $list)->forceDelete();
+
+            return response()->json([
+                'type' => 'success',
+                'title' => 'Users',
+                'message' => '{0} Nothing to erase.|[1] Item erased successfully.|[2,*] :total items successfully erased.',
+                'length' => $total,
+                'replacements' => ['total' => $total],
+            ]);
+        } catch (\Throwable $e) {
+            report($e);
+
+            return response()->json([
+                'type' => 'error',
+                'message' => 'Error on erase selected item.|Error on erase selected items.',
+                'length' => $list,
+            ]);
+        }
+    }
+
+    public function __fields(Request $request): array
+    {
+        return [
+            [
+                'type' => 'input',
+                'name' => 'name',
+                'label' => 'Name',
+                'span' => 2,
+            ],
+            [
+                'type' => 'toggle',
+                'name' => 'active',
+                'label' => 'Active',
+                'colorOn' => 'success',
+                'colorOff' => 'danger',
+            ],
+            [
+                'type' => 'input',
+                'name' => 'email',
+                'label' => 'Email',
+                'span' => 2,
+            ],
+            [
+                'type' => 'text',
+                'name' => 'email_verified_at',
+                'label' => 'Email verified at',
+                'readonly' => true,
+            ],
+        ];
+    }
+
+    public function index2(Request $request): Response
+    {
+        $this->authorize('access', User::class);
+
+        return Inertia::render('Bood4ll', [
+            'build' => [
+                [
+                    'label' => Route::current()->title,
+                    'description' => Route::current()->description,
+                    'fields' => [
+                        [
+                            'type' => 'table',
+                            'structure' => [
+                                'actions' => [
+                                    'index' => [
+                                        'source' => 'getHolidaysIndex',
+                                        'visible' => Gate::allows('apps.holidays.index'),
+                                        'disabled' => $request->user()->cannot('isManager', User::class),
+                                    ],
+                                    'create' => [
+                                        'visible' => (
+                                            Gate::allows('apps.holidays.store')
+                                            && $request->user()->can('isManager', User::class)
+                                        ),
+                                        'disabled' => $request->user()->cannot('isManager', User::class),
+                                        'components' => [
+                                            [
+                                                'label' => 'Main data',
+                                                'description' => 'User account profile information.',
+                                                'cols' => 3,
+                                                'fields' => $this->__fields($request),
+                                                'visible' => (
+                                                    Gate::allows('apps.holidays.store')
+                                                    && $request->user()->can('isManager', User::class)
+                                                ),
+                                                'disabled' => $request->user()->cannot('isManager', User::class),
+                                                'confirm' => true,
+                                                'toastTitle' => 'Add',
+                                                'toast' => '{0} Nothing to add.|[1] Item added successfully.|[2,*] :total items successfully added.',
+                                                'toastClass' => 'success',
+                                                'callback' => 'apps.holidays.store',
+                                                'method' => 'post',
+                                            ],
+                                        ],
+                                    ],
+                                    'edit' => [
+                                        'visible' => (
+                                            Gate::allows('apps.holidays.update')
+                                            && $request->user()->can('isManager', User::class)
+                                            && $request->user()->can('canManageNestedData', User::class)
+                                        ),
+                                        'disabled' => $request->user()->cannot('isManager', User::class),
+                                        'components' => [
+                                            [
+                                                'source' => [
+                                                    'route' => 'getUserInfo',
+                                                    'transmute' => ['user' => 'id'],
+                                                ],
+                                                'label' => 'Main data',
+                                                'description' => 'User account profile information.',
+                                                'cols' => 3,
+                                                'fields' => $this->__fields($request),
+                                                'visible' => (
+                                                    Gate::allows('apps.holidays.update')
+                                                    && $request->user()->can('isManager', User::class)
+                                                    && $request->user()->can('canManageNestedData', User::class)
+                                                ),
+                                                'disabled' => $request->user()->cannot('isManager', User::class),
+                                                'confirm' => true,
+                                                'toastTitle' => 'Edit',
+                                                'toast' => '{0} Nothing to edit.|[1] Item edited successfully.|[2,*] :total items successfully edited.',
+                                                'toastClass' => 'success',
+                                                'callback' => 'apps.holidays.update',
+                                                'method' => 'patch',
+                                            ],
+                                        ],
+                                    ],
+                                    'destroy' => [
+                                        'callback' => 'apps.holidays.destroy',
+                                        'method' => 'delete',
+                                        'visible' => (
+                                            Gate::allows('apps.holidays.destroy')
+                                            && $request->user()->can('isManager', User::class)
+                                            && $request->user()->can('canManageNestedData', User::class)
+                                        ),
+                                    ],
+                                    'restore' => [
+                                        'callback' => 'apps.holidays.restore',
+                                        'method' => 'post',
+                                        'visible' => (
+                                            Gate::allows('apps.holidays.restore')
+                                            && $request->user()->can('isManager', User::class)
+                                            && $request->user()->can('canManageNestedData', User::class)
+                                        ),
+                                    ],
+                                    'forceDestroy' => [
+                                        'callback' => 'apps.holidays.forceDestroy',
+                                        'method' => 'delete',
+                                        'visible' => (
+                                            Gate::allows('apps.holidays.forceDestroy')
+                                            && $request->user()->can('isManager', User::class)
+                                            && $request->user()->can('canManageNestedData', User::class)
+                                        ),
+                                    ],
+                                ],
+                                'titles' => [
+                                    [
+                                        'type' => 'composite',
+                                        'header' => 'Name',
+                                        'field' => 'name',
+                                        'values' => [
+                                            [
+                                                'field' => 'name',
+                                            ],
+                                            [
+                                                'field' => 'authority',
+                                                'class' => 'text-xs',
+                                            ],
+                                        ],
+                                    ],
+                                    [
+                                        'type' => 'active',
+                                        'header' => 'Day off',
+                                        'field' => 'day_off',
+                                    ],
+                                    [
+                                        'type' => 'composite',
+                                        'header' => 'Duration',
+                                        'field' => 'start_at',
+                                        'values' => [
+                                            [
+                                                'field' => 'start_at',
+                                                'class' => 'text-xs',
+                                            ],
+                                            [
+                                                'field' => 'end_at',
+                                                'class' => 'text-xs',
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ]);
     }
 }
--- app/routes/api.php
+++ app/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Authorization\RolesController;
 use App\Http\Controllers\Authorization\UnitsController;
 use App\Http\Controllers\Authorization\UsersController;
+use App\Http\Controllers\HolidaysController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,25 +11,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(RolesController::class)->middleware('auth:sanctum', 'verified', 'password.confirm')->group(function () {
-    Route::get('/getAbilities', 'getAbilities')->name('getAbilities');
-    Route::get('/getAbilitiesIndex', 'getAbilitiesIndex')->name('getAbilitiesIndex');
-    Route::put('/putAbilitiesUpdate/{mode?}', 'putAbilitiesUpdate')->name('putAbilitiesUpdate')->whereIn('mode', ['toggle', 'on', 'off']);
-    Route::get('/getRolesIndex', 'getRolesIndex')->name('getRolesIndex');
-    Route::get('/getRoleInfo/{role}', 'getRoleInfo')->name('getRoleInfo');
-    Route::get('/getRoleAuthorized/{role}/{show?}', 'getRoleAuthorized')->name('getRoleAuthorized');
-});
+Route::middleware('auth:sanctum', 'verified', 'password.confirm')->group(function () {
+    Route::controller(RolesController::class)->group(function () {
+        Route::get('/getAbilities', 'getAbilities')->name('getAbilities');
+        Route::get('/getAbilitiesIndex', 'getAbilitiesIndex')->name('getAbilitiesIndex');
+        Route::put('/putAbilitiesUpdate/{mode?}', 'putAbilitiesUpdate')->name('putAbilitiesUpdate')->whereIn('mode', ['toggle', 'on', 'off']);
+        Route::get('/getRolesIndex', 'getRolesIndex')->name('getRolesIndex');
+        Route::get('/getRoleInfo/{role}', 'getRoleInfo')->name('getRoleInfo');
+        Route::get('/getRoleAuthorized/{role}/{show?}', 'getRoleAuthorized')->name('getRoleAuthorized');
+    });
 
-Route::controller(UnitsController::class)->middleware('auth:sanctum', 'verified', 'password.confirm')->group(function () {
-    Route::get('/getUnits/{unit?}', 'getUnits')->name('getUnits');
-    Route::get('/getUnitsIndex', 'getUnitsIndex')->name('getUnitsIndex');
-    Route::get('/getUnitInfo/{unit?}', 'edit')->name('getUnitInfo');
-    Route::get('/getUnitStaff/{unit?}/{show?}', 'getUnitStaff')->name('getUnitStaff');
-});
+    Route::controller(UnitsController::class)->group(function () {
+        Route::get('/getUnits/{unit?}', 'getUnits')->name('getUnits');
+        Route::get('/getUnitsIndex', 'getUnitsIndex')->name('getUnitsIndex');
+        Route::get('/getUnitInfo/{unit?}', 'edit')->name('getUnitInfo');
+        Route::get('/getUnitStaff/{unit?}/{show?}', 'getUnitStaff')->name('getUnitStaff');
+    });
 
-Route::controller(UsersController::class)->middleware('auth:sanctum', 'verified', 'password.confirm')->group(function () {
-    Route::get('/getUsersIndex', 'getUsersIndex')->name('getUsersIndex');
-    Route::get('/getUserInfo/{user?}', 'getUserInfo')->name('getUserInfo');
-    Route::get('/getUserUnits/{user?}/{show?}', 'getUserUnits')->name('getUserUnits');
-    Route::get('/getUserRoles/{user?}/{show?}', 'getUserRoles')->name('getUserRoles');
+    Route::controller(UsersController::class)->group(function () {
+        Route::get('/getUsersIndex', 'getUsersIndex')->name('getUsersIndex');
+        Route::get('/getUserInfo/{user?}', 'getUserInfo')->name('getUserInfo');
+        Route::get('/getUserUnits/{user?}/{show?}', 'getUserUnits')->name('getUserUnits');
+        Route::get('/getUserRoles/{user?}/{show?}', 'getUserRoles')->name('getUserRoles');
+    });
+
+    Route::controller(HolidaysController::class)->group(function () {
+        Route::get('/getHolidaysIndex', 'getHolidaysIndex')->name('getHolidaysIndex');
+    });
 });
